97ca5820dfe412ac9c4432704bb56dcaf9d95635
heenbo <379667345@qq.com>
2016-06-13 10:22:33 +0800

    mosquitto_client.c test_sub

--- client/mosquitto_client.c
+++ client/mosquitto_client.c
@@ -8,9 +8,37 @@
 #include <stdio.h>
 #include "mosquitto_client.h"
 
+#define CA_FILE_PATH 	"/home/heenbo/work/openssl_CA/ca.crt"
+#define CERT_FILE_PATH 	"/home/heenbo/work/openssl_CA/client.crt"
+#define KEY_FILE_PATH 	"/home/heenbo/work/openssl_CA/client.key"
+#define HOST_IP 	"192.168.199.244"
+#define HOST_PORT	"8883"
+#define TOPIC_01	"mqtt"
+#define USER_NAME	"001"
+#define USER_PASSWD	"001"
+#define CLIENT_ID	"0001"
+
+char * test_sub[] = {
+	"mosquitto_client",
+	"-h", HOST_IP,
+	"-p", HOST_PORT,
+	"-t", TOPIC_01,
+	"--cafile", CA_FILE_PATH,
+	"--cert", CERT_FILE_PATH,
+	"--key", KEY_FILE_PATH,
+	"-u", USER_NAME,
+	"-P", USER_PASSWD,
+	"-i", CLIENT_ID,
+	
+};
+
 int main(int argc, char *argv[])
 {
 	printf("test001\n");
+
+	main_mosquitto_sub(19, test_sub);
+//main_mosquitto_sub(argc, char *argv[]);
+//main_mosquitto_pub(int argc, char *argv[]);
 
 
 
